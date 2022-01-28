2eee03deff10b15e4322765bdc257db6ecc89362
pauloALuis <50427097+pauloALuis@users.noreply.github.com>
2022-01-28 14:51:48 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+namespace ExerciseFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string sourcePath = @"D:\Documents\curso\file1.txt";
+            string targetPath = @"E:\files\file3.txt";
+
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                Console.WriteLine(fileInfo.Exists);
+
+                var mess = fileInfo.CopyTo(targetPath);
+                Console.WriteLine(mess);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
